54ec6034e49197734e7fc410570eec00215f7c94
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2024-06-08 11:37:56 -0600

    refactored 2-data-processing scipt

--- src/2-data-processing.py
+++ src/2-data-processing.py
@@ -1,35 +1,37 @@
-# TODO: design categoricla features vocab
-# TODO: preprocess genres and titles
-
 # %% Imports
-from typing import List
 import gensim
 import pandas as pd
 import numpy as np
 import gensim.downloader
 import json
 import re
+import logging
+
 from sklearn.preprocessing import MultiLabelBinarizer
 from gensim.parsing.preprocessing import *
+from typing import List, Dict
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 
 # %% Data loading
+def load_data(file_path: str) -> pd.DataFrame:
+    """_summary_
 
-data = pd.read_parquet("../local_data/full_data.parquet")
-data.head()
+    Args:
+        file_path (str): _description_
 
-data = data.sort_values(by="timestamp", ascending=True).reset_index(drop=True)
-data["is_test"] = data.index % 20 == 0
-
-print(f"Test split has {len(data[data['is_test'] == 1])} rows")
-
-# %% Preprocessing genres
-mlb = MultiLabelBinarizer()
-genres_encoded = mlb.fit_transform(data.movie_genre)
-genres_encoded_df = pd.DataFrame(genres_encoded, columns=mlb.classes_)
-genres_encoded_df
-
-#! Possible issue here with mismatched colums. Come back here if issues.
-genre_columns = genres_encoded_df.columns.values
+    Returns:
+        pd.DataFrame: _description_
+    """
+    logging.info("Loading data from parquet file.")
+    data = pd.read_parquet(file_path)
+    data = data.sort_values(by="timestamp", ascending=True).reset_index(drop=True)
+    data["is_test"] = data.index % 20 == 0
+    logging.info(f"Test split has {len(data[data['is_test'] == 1])} rows")
+    return data
 
 
 def clean_genres(genre):
@@ -38,17 +40,35 @@ def clean_genres(genre):
     return f"genre_{genre}"
 
 
-genre_columns_clean = [clean_genres(genre) for genre in genre_columns]
-genres_encoded_df.columns = genre_columns_clean
+def preprocess_genres(data: pd.DataFrame) -> pd.DataFrame:
+    logging.info("Preprocessing movie genres.")
+    mlb = MultiLabelBinarizer()
+    genres_encoded = mlb.fit_transform(data.movie_genre)
+    genres_encoded_df = pd.DataFrame(genres_encoded, columns=mlb.classes_)
+    #! Possible issue here with mismatched colums. Come back here if issues.
+    genre_columns = genres_encoded_df.columns.values
 
-data = pd.concat([data, genres_encoded_df], axis=1)
+    genre_columns_clean = [clean_genres(genre) for genre in genre_columns]
+    assert all(
+        column.startswith("genre_") for column in genre_columns_clean
+    ), "Genre columns not correctly encoded."
 
-# %% Processing titles
-word2vec = gensim.downloader.load("glove-twitter-25")
+    genres_encoded_df.columns = genre_columns_clean
+
+    data = pd.concat([data, genres_encoded_df], axis=1)
+
+    return data
 
 
-# %%
-def safe_word_embed(word: string, length=25) -> np.array:
+def load_word2vec_model() -> gensim.models.KeyedVectors:
+    logging.info("Loading GloVe model")
+    word2vec = gensim.downloader.load("glove-twitter-25")
+    return word2vec
+
+
+def safe_word_embed(
+    word: string, model: gensim.models.KeyedVectors, length=25
+) -> np.array:
     """Function to safely embed a word as the model requires words to be
     lower cased.
     Args:
@@ -59,8 +79,9 @@ def safe_word_embed(word: string, length=25) -> np.array:
         np.array: the embedding vector
     """
     try:
-        return word2vec[word.lower()]
+        return model[word.lower()]
     except:
+        logging.warning(f"Word {word} not found in GloVe model. Returning zeros.")
         return np.zeros(length)
 
 
@@ -74,9 +95,9 @@ def gensim_embed_processing(title: string) -> List[str]:
     return title.split(" ")
 
 
-def embed_title(title: string) -> pd.Series:
+def embed_title(title: string, model: gensim.models.KeyedVectors) -> pd.Series:
     words = gensim_embed_processing(title)
-    word_embeddings = np.array([safe_word_embed(word) for word in words])
+    word_embeddings = np.array([safe_word_embed(word, model) for word in words])
     title_embeddings = word_embeddings.sum(axis=0)
 
     return pd.Series(
@@ -87,41 +108,83 @@ def embed_title(title: string) -> pd.Series:
     )
 
 
-embeddings_df = data.movie_title.apply(embed_title)
-data = pd.concat([data, embeddings_df], axis=1)
-print(data.columns)
+def process_titles(
+    data: pd.DataFrame, model: gensim.models.KeyedVectors
+) -> pd.DataFrame:
+    logging.info("Embedding movie titles.")
+    embeddings_df = data.movie_title.apply(embed_title, model=model)
 
-# %% Test tran split
-data = data.drop(columns=["user_zip", "timestamp", "movie_title"])
-test_data = data[data.is_test].drop(columns="is_test")
-train_data = data[~data.is_test].drop(columns="is_test")
-
-print(test_data.shape)
-print(train_data.shape)
+    return pd.concat([data, embeddings_df], axis=1)
 
 
-test_data.to_csv("../local_data/test_data.csv", index=False)
-train_data.to_csv("../local_data/train_data.csv", index=False)
+def train_test_split(data: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    logging.info("Splitting data into test and train sets.")
+    data = data.drop(columns=["user_zip", "timestamp", "movie_title"])
+    test_data = data[data.is_test].drop(columns="is_test")
+    train_data = data[~data.is_test].drop(columns="is_test")
+
+    return train_data, test_data
+
+
+def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame) -> None:
+    test_data.to_csv("local_data/test_data.csv", index=False)
+    train_data.to_csv("local_data/train_data.csv", index=False)
+    logging.info("Training and testing data saved to CSV files.")
+
 
 # %% Metadata for the model. See eda.ipynb long-tail distribution section
-metadata = dict()
-metadata["title_embedding_size"] = 25  # size of embedding of glove-twitter-25
-metadata["na_string"] = "XX"
-metadata["genres"] = genre_columns_clean
-metadata["ages"] = data.user_age.unique().tolist()
-metadata["occupations"] = data.user_job.unique().tolist()
+def create_metadata(data: pd.DataFrame, title_embedding_size: int = 25) -> Dict:
+    logging.info("Creating metadata for the model.")
+    metadata = dict()
+    metadata["title_embedding_size"] = (
+        title_embedding_size  # size of embedding of glove-twitter-25
+    )
+    metadata["na_string"] = "XX"
+    metadata["genres"] = data.columns[data.columns.str.startswith("genre_")].to_list()
+    metadata["ages"] = data.user_age.unique().tolist()
+    metadata["occupations"] = data.user_job.unique().tolist()
 
-metadata["user_id"] = data.user_id.value_counts().index.to_list()[:2500]
-metadata["movie_id"] = data.movie_id.value_counts().index.to_list()[:1200]
-metadata["user_city"] = (
-    data.user_city[~(data.user_city == "XX")].value_counts().index.to_list()[:500]
-)
-metadata["user_state"] = (
-    data.user_state[~(data.user_state == "XX")].value_counts().index.to_list()[:20]
-)
+    metadata["user_id"] = data.user_id.value_counts().index.to_list()[:2500]
+    metadata["movie_id"] = data.movie_id.value_counts().index.to_list()[:1200]
+    metadata["user_city"] = (
+        data.user_city[~(data.user_city == "XX")].value_counts().index.to_list()[:500]
+    )
+    metadata["user_state"] = (
+        data.user_state[~(data.user_state == "XX")].value_counts().index.to_list()[:20]
+    )
+    logging.info("Metadata creation completed.")
+    return metadata
 
-# %% save the metadata to json file
 
-with open("../local_data/metadata.json", "w+") as file:
-    json.dump(metadata, file)
-# %%
+def save_metadata(metadata: Dict, file_path: str) -> None:
+    logging.info("Saving metadata to JSON file.")
+    with open(file_path, "w+") as file:
+        json.dump(metadata, file)
+    logging.info("Saved metadata to JSON file.")
+
+
+if __name__ == "__main__":
+    DATA_PATH = "local_data/full_data.parquet"
+    METADATA_PATH = "local_data/metadata.json"
+
+    data = load_data(DATA_PATH)
+    assert not data.empty, "Data loading failed, dataframe is empty"
+
+    data = preprocess_genres(data)
+
+    word2vec_model = load_word2vec_model()
+
+    data = process_titles(data, word2vec_model)
+
+    train_data, test_data = train_test_split(data)
+    assert (
+        not train_data.empty and not test_data.empty
+    ), "Data split failed, one of the datasets is empty"
+
+    logging.info(f"Training data shape: {train_data.shape}")
+    logging.info(f"Test data shape: {test_data.shape}")
+
+    save_data(train_data, test_data)
+
+    metadata = create_metadata(data)
+    save_metadata(metadata, METADATA_PATH)
